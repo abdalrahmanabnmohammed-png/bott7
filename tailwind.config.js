b5dcc69d2b37212e93a0905465e29b6c16f5119a
abdalrahmanabnmohammed-png <abdalrahman.abn.mohammed@gmail.com>
2026-02-15 03:46:54 -0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'jo-purple': '#A62DC9',
+      },
+      animation: {
+        'fade-in': 'fadeIn 1.2s ease-out forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0', transform: 'translateY(10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        }
+      },
+    },
+  },
+  plugins: [],
+}
